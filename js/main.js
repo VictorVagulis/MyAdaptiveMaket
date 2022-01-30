7aa239e91e4f11d8cf855cca6ea556c5345339e2
Viktor Vagulis <mrnutscase123@gmail.com>
2022-01-30 17:31:41 +0300

    complete accordeon

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 }
@@ -26,80 +26,26 @@ document.addEventListener('keydown', function(e) {
     }
 });
 
+const tab = document.querySelectorAll(".characteristics");
+const tCont = document.querySelectorAll(".descr");
 
-// class ItcTabs {
-//     constructor(target, config) {
-//         const defaultConfig = {};
-//         this._config = Object.assign(defaultConfig, config);
-//         this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
-//         this._elButtons = this._elTabs.querySelectorAll('.characteristics');
-//         this._elPanes = this._elTabs.querySelectorAll('.descr');
-//         this._eventShow = new Event('tab.itc.change');
-//         this._init();
-//         this._events();
-//     }
-//
-//     _init() {
-//         this._elTabs.setAttribute('role', 'tablist');
-//         this._elButtons.forEach((el, index) => {
-//             el.dataset.index = index;
-//             el.setAttribute('role', 'tab');
-//             this._elPanes[index].setAttribute('role', 'tabpanel');
-//         });
-//     }
-//
-//     show(elLinkTarget) {
-//         const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
-//         const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
-//         const elPaneShow = this._elTabs.querySelector('.tabs_pane_show');
-//         if (elLinkTarget === elLinkActive) {
-//             return;
-//         }
-//         elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
-//         elPaneShow ? elPaneShow.classList.remove('tabs_pane_show') : null;
-//         elLinkTarget.classList.add('tabs__btn_active');
-//         elPaneTarget.classList.add('tabs_pane_show');
-//         this._elTabs.dispatchEvent(this._eventShow);
-//         elLinkTarget.focus();
-//     }
-//
-//     showByIndex(index) {
-//         const elLinkTarget = this._elButtons[index];
-//         elLinkTarget ? this.show(elLinkTarget) : null;
-//     };
-//
-//     _events() {
-//         this._elTabs.addEventListener('click', (e) => {
-//             const target = e.target.closest('.characteristics');
-//             if (target) {
-//                 e.preventDefault();
-//                 this.show(target);
-//             }
-//         });
-//     }
-// }
-// new ItcTabs('.characteristics-reviews');
+tab.forEach(function (item) {
+     item.addEventListener("click", function () {
+         let currentBtn = item;
+         let tabCont = currentBtn.getAttribute("data_tab");
+         let cont = document.querySelector(tabCont);
 
+         if ( ! currentBtn.classList.contains('active')){
 
-let tab = function () {
-    let tabNav = document.querySelectorAll('.description'),
-           tabContenr = document.querySelectorAll('.descr'),
-           tabName;
-       tabNav.forEach(item => {item.addEventListener('click',    selectTabNav)});
-    function selectTabNav() {
-           tabNav.forEach(item => {
-               item.classList.remove('tabs_btn_active');
-           });
-       this.classList.add('tabs_btn_active');
-       tabName = this.getAttribute('data-tab-name');
-       selectTabContent(tabName);
-    }
-    function selectTabContent(tabName) {
-        tabContenr.forEach(item => {
-            item.classList.contains(tabName) ? item.classList.add('tabs_btn_active') : item.classList.remove('tabs_btn_active');
-        })
-    }
-};
+         tab.forEach(function (item) {
+            item.classList.remove('active');
+         });
+         tCont.forEach(function (item) {
+            item.classList.remove('active');
+         });
 
-tab();
-
+         currentBtn.classList.add('active');
+         cont.classList.add('active');
+         }
+     });
+});
