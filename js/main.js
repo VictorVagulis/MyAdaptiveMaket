9b0ee296cb666da3d8cbcbb881845a2170608b9c
Viktor Vagulis <mrnutscase123@gmail.com>
2022-02-26 14:34:12 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -57,3 +57,55 @@ tab.forEach(function (item) {
 
 //slideShow
 
+let position = 0;
+const slidesToShow = 4;
+const slidesToScroll = 1;
+const container = document.querySelector(".product-slider");
+const track = document.querySelector(".best-groods_products");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+const items = document.querySelectorAll(".product-1");
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+
+btnNext.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft > slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition();
+    });
+
+btnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    position += itemsLeft > slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition();
+});
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`
+    checkBtns();
+};
+const checkBtns = () => {
+
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+    btnPrev.disabled = position === 0;
+
+    btnNextDis();
+};
+
+const btnNextDis = () => {
+    if (btnNext.disabled) {
+        btnNext.classList.remove('btn-next');
+    } else {
+        btnNext.classList.add('btn-next');
+    }
+    if (btnPrev.disabled) {
+        btnPrev.classList.remove('btn-prev');
+    } else {
+        btnPrev.classList.add('btn-prev');
+    }
+};
+
